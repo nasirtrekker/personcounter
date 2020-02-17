bf3a67b310f804acdfb02058a4abc6763829dc18
Nasir Uddin <nasirtrekker@users.noreply.github.com>
2020-02-17 16:09:15 +0100

    Update footcounter.py

--- footcounter.py
+++ footcounter.py
@@ -1,11 +1,11 @@
 # USAGE
 # To read and write back out to video:
-# python people_counter.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt \
+# python footcounter.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt \
 #	--model mobilenet_ssd/MobileNetSSD_deploy.caffemodel --input videos/example_01.mp4 \
 #	--output output/output_01.avi
 #
 # To read from webcam and write back out to disk:
-# python people_counter.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt \
+# python footcounter.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt \
 #	--model mobilenet_ssd/MobileNetSSD_deploy.caffemodel \
 #	--output output/webcam_output1.avi
 
